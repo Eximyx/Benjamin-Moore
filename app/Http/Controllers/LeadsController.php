8e7f984df19e599acac5e7ef3b081736fd651910
EriCHek15 <eriksokol2@gmail.com>
2024-01-02 14:12:57 +0300

    wd

--- app/Http/Controllers/LeadsController.php
+++ app/Http/Controllers/LeadsController.php
@@ -2,64 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Leads;
 use App\Models\LeadsMigration;
 use Illuminate\Http\Request;
 
 class LeadsController extends BaseController
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(LeadsMigration $leadsMigration)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(LeadsMigration $leadsMigration)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, LeadsMigration $leadsMigration)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(LeadsMigration $leadsMigration)
-    {
-        //
+    public function __construct(Leads $model) {
+        parent::__construct($model);    
     }
 }
