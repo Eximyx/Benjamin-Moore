bf6d5beca94d145141209ab202e11ec5134e60a2
EriCHek15 <eriksokol2@gmail.com>
2024-01-20 13:54:32 +0300

    форматирование кода

--- app/Http/Requests/CreateLeadsRequest.php
+++ app/Http/Requests/CreateLeadsRequest.php
@@ -6,24 +6,16 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CreateLeadsRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'id' => 'numeric|nullable',
-            'name' => 'string|required', 
+            'name' => 'string|required',
             'contactInfo' => 'string|required',
             'message' => 'string|required',
         ];
