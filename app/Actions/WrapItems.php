c4e56fc824e1dc33b3d5796faf3c5cf7276bd77f
Eximyx <mr.vitalik.12352@gmail.com>
2024-02-08 15:49:33 +0300

    Laravel Pint + WrapItems fixes

--- app/Actions/WrapItems.php
+++ app/Actions/WrapItems.php
@@ -3,9 +3,15 @@
 namespace App\Actions;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 class WrapItems
 {
+    /**
+     * @param Collection<int,Model> $items
+     * @param int $slideAmount
+     * @return array<int,array<Model>>
+     */
     public function __invoke(Collection $items, int $slideAmount): array
     {
         $j = 0;
