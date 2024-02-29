dcb05127205dde9ada6d1d2893571090b9feeea6
Eximyx <mr.vitalik.12352@gmail.com>
2024-02-29 10:05:29 +0300

    Update 2024_02_26_132350_create_meta_data_table.php

--- database/migrations/2024_02_26_132350_create_meta_data_table.php
+++ database/migrations/2024_02_26_132350_create_meta_data_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('meta_data', function (Blueprint $table) {
-            $table->string('url')->unique();
+            $table->string('url')->unique()->primary();
             $table->string('title')->nullable();
             $table->string('meta_description')->nullable();
             $table->string('meta_keywords')->nullable();
