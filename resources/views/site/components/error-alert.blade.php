b103e2746304a9b577a9726258aadb1950450ecd
frynn <grodnenec04@mail.ru>
2024-03-22 10:37:41 +0300

    Implemented alerts for lead form

---
+++ resources/views/site/components/error-alert.blade.php
@@ -0,0 +1,7 @@
+<div class="modal-blurred">
+    <div class="modal-block">
+        <img src="{{Vite::asset('resources/icons/error.svg')}}" alt="error">
+        {{--            TODO: lang for message--}}
+        <p class="alert-message">Something went wrong ;(</p>
+    </div>
+</div>
