b0ab97f08659a15d9082b92250a75b17e91d5ef6
frynn <grodnenec04@mail.ru>
2024-03-22 10:37:41 +0300

    Implemented alerts for lead form

---
+++ resources/views/site/components/success-alert.blade.php
@@ -0,0 +1,7 @@
+<div class="modal-blurred success">
+    <div class="modal-block success">
+        <img src="{{Vite::asset('resources/icons/success.svg')}}" alt="success">
+        {{--            TODO: lang for close button and message--}}
+        <p class="alert-message">Message successfully sent!</p>
+    </div>
+</div>
