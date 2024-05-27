33ab9f84146c74f4620cb863f3179dbf5c3ad711
EriCHek15 <eriksokol2@gmail.com>
2024-05-27 15:52:10 +0300

    langs for errors

--- lang/en/errors.php
+++ lang/en/errors.php
@@ -2,10 +2,10 @@
 
 return [
     'banners' => [
-        'position' => 'Error, you cannot delete a banner that has a position',
+        'position' => 'Error, you cannot delete a banner which is posted on the site',
     ],
     'sections' => [
-        'position' => 'Error, you cannot delete a section that has a position',
+        'position' => 'Error, you cannot delete a section which is posted on the site',
     ],
     'auth' => [
         'failed' => 'Error, you are not authorized',
@@ -14,8 +14,8 @@ return [
         'categories' => 'Error, you cannot delete a category that has a posts',
     ],
     'products' => [
-        'colors' => 'Error, you cannot delete a color that has a products',
-        'categories' => 'Error, you cannot delete a category that has a products',
+        'colors' => 'Error, you cannot delete a color which is associated with the product',
+        'categories' => 'Error, you cannot delete a category which is associated with the product',
     ]
 
 ];
